4bb3b7b7645f974c1209f675d3ce4a6140e9da5b
NurbolNurlanov12 <114813165+StarForceGames@users.noreply.github.com>
2024-03-06 11:05:55 +0300

    Update fortnite.py

--- fortnite.py
+++ fortnite.py
@@ -1,3 +1,3 @@
 for i in range(100, 1000): #Берем все 3 значные числа
     if (str(i)[0] != str(i)[1]) and (str(i)[2] != str(i)[1]) and (str(i)[0] != str(i)[2]):
-        print(i,end = ' ')
+        print(i,end = ',')
